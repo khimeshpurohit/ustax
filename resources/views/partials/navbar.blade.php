eb71383489e2703cb25e4fb9b6f1fc669dac2f5f
parth_thaker <parththaker1991@gmail.com>
2025-07-10 22:13:30 +0530

    home page animation

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -9,11 +9,11 @@
 
       <!-- Desktop Nav -->
       <nav class="hidden lg:flex items-center space-x-6 text-base">
-        <a href={{ route('home') }}  class="text-primary active"  wire:navigate>Home</a>
-        <a  href={{ route('about') }} class="text-black hover:text-primary"  wire:navigate>About Us</a>
+        <a href={{ route('home') }}  class="nav-link text-primary active"  wire:navigate>Home</a>
+        <a  href={{ route('about') }} class="nav-link text-black hover:text-primary"  wire:navigate>About Us</a>
 
         <!-- Dropdown Wrapper -->
-        <div class="dropdown relative group">
+        <div class="nav-link dropdown relative group">
           <button class="dropdown-toggle flex items-center text-black hover:text-primary cursor-pointer">
             Services
             <svg class="icon ml-1 w-4 h-4 transition-transform" fill="none" stroke="currentColor" stroke-width="2"
@@ -22,14 +22,13 @@
             </svg>
           </button>
           <div class="dropdown-panel absolute top-10 left-0 w-52 bg-white shadow-md border rounded hidden z-10">
-            <a href={{ route('individual-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100">Individual Tax Fill</a>
-            <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100">Business Tax Fill</a>
+            
 
             <!-- Nested Dropdown -->
             <div class="dropdown relative group">
               <button
                 class="dropdown-toggle flex justify-between items-center w-full px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                More Services
+                Individual Tax Fill
                 <svg class="icon ml-2 w-3 h-3 transition-transform" fill="none" stroke="currentColor" stroke-width="2"
                   viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
@@ -37,17 +36,39 @@
               </button>
               <div
                 class="dropdown-panel absolute left-full top-0 mt-0 ml-1 w-48 bg-white border rounded shadow-md hidden">
-                <a href={{ route('payroll') }} class="block px-4 py-2 hover:bg-gray-100">Payroll</a>
-                <a href={{ route('book-keeping') }} class="block px-4 py-2 hover:bg-gray-100">Bookkeeping</a>
+                <a href={{ route('individual-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>Federal and State Tax Filing</a>
+                <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>FBAR Processing & FATCA Processing</a>
+                <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>ITIN Processing Services</a>
+                <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>Tax Representation Services</a>
+                <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>Amendment Filing Services     </a>
+                <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>Indian Tax Filing Services</a>
+                <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>Advanced Tax Planning Services</a>
+              </div>
+            </div>
+            <!-- Nested Dropdown -->
+            <div class="dropdown relative group">
+              <button
+                class="dropdown-toggle flex justify-between items-center w-full px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                Business Tax Fill
+                <svg class="icon ml-2 w-3 h-3 transition-transform" fill="none" stroke="currentColor" stroke-width="2"
+                  viewBox="0 0 24 24">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+                </svg>
+              </button>
+              <div
+                class="dropdown-panel absolute left-full top-0 mt-0 ml-1 w-48 bg-white border rounded shadow-md hidden">
+                <a href={{ route('individual-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>Entity Formation Services</a>
+                <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>Corporate Tax Filing Services</a>
+                <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>Limited Liability Company (LLC) Services</a>
               </div>
             </div>
           </div>
         </div>
 
 
-        <a href={{ route('refund') }} class="text-black hover:text-primary">Refund</a>
-        <a href={{ route('referral') }} class="text-black hover:text-primary">Referral Program</a>
-        <a href={{ route('contact') }} class="text-black hover:text-primary">Contact Us</a>
+        <a href={{ route('refund') }} class="nav-link text-black hover:text-primary">Refund</a>
+        <a href={{ route('referral') }} class="nav-link text-black hover:text-primary">Referral Program</a>
+        <a href={{ route('contact') }} class="nav-link text-black hover:text-primary">Contact Us</a>
       </nav>
       <div class="hidden lg:flex space-x-3">
                 @auth
@@ -195,11 +216,11 @@
       </button>
     </div>
     <div class="p-6 space-y-4 text-sm">
-      <a href={{ route('home') }} class="block text-primary " wire:navigate>Home</a>
-      <a href={{ route('about') }} class="block text-black" wire:navigate>About Us</a>
+      <a href={{ route('home') }} class="nav-link block text-primary " wire:navigate>Home</a>
+      <a href={{ route('about') }} class="nav-link block text-black" wire:navigate>About Us</a>
 
       <!-- Mobile Dropdown -->
-      <div class="dropdown">
+      <div class="nav-link dropdown">
         <button class="dropdown-toggle w-full text-left flex justify-between items-center text-black py-2">
           Services
           <svg class="icon w-4 h-4 transition-transform" fill="none" stroke="currentColor" stroke-width="2"
@@ -208,21 +229,40 @@
           </svg>
         </button>
         <div class="dropdown-panel pl-4 mt-2 space-y-2 hidden">
-          <a href={{ route('individual-tax-fill') }} class="block text-gray-600 " wire:navigate>Individual Tax Fill</a>
-          <a href={{ route('business-tax-fill') }} class="block text-gray-600" wire:navigate>Business Tax Fill</a>
+         
 
           <!-- Nested Dropdown -->
           <div class="dropdown">
             <button class="dropdown-toggle w-full text-left flex justify-between items-center text-gray-600 py-2">
-              More Services
+              Individual Tax Fill
               <svg class="icon w-4 h-4 transition-transform" fill="none" stroke="currentColor" stroke-width="2"
                 viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
               </svg>
             </button>
             <div class="dropdown-panel pl-4 mt-2 space-y-2 hidden">
-              <a href={{ route('payroll') }} class="block text-gray-600" wire:navigate>Payroll</a>
-              <a href={{ route('book-keeping') }} class="block text-gray-600" wire:navigate>Bookkeeping</a>
+              <a href={{ route('individual-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>Federal and State Tax Filing</a>
+                <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>FBAR Processing & FATCA Processing</a>
+                <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>ITIN Processing Services</a>
+                <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>Tax Representation Services</a>
+                <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>Amendment Filing Services     </a>
+                <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>Indian Tax Filing Services</a>
+                <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>Advanced Tax Planning Services</a>
+            </div>
+          </div>
+          <!-- Nested Dropdown -->
+          <div class="dropdown">
+            <button class="dropdown-toggle w-full text-left flex justify-between items-center text-gray-600 py-2">
+              Business Tax Fill
+              <svg class="icon w-4 h-4 transition-transform" fill="none" stroke="currentColor" stroke-width="2"
+                viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+              </svg>
+            </button>
+            <div class="dropdown-panel pl-4 mt-2 space-y-2 hidden">
+                 <a href={{ route('individual-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>Entity Formation Services</a>
+                <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>Corporate Tax Filing Services</a>
+                <a href={{ route('business-tax-fill') }} class="block px-4 py-2 hover:bg-gray-100" wire:navigate>Limited Liability Company (LLC) Services</a>
             </div>
           </div>
         </div>
@@ -230,9 +270,9 @@
 
 
 
-      <a href={{ route('refund') }} class="block text-black">Refund</a>
-      <a href={{ route('referral') }} class="block text-black">Referral Program</a>
-      <a href={{ route('contact') }} class="block text-black">Contact Us</a>
+      <a href={{ route('refund') }} class="nav-link block text-black">Refund</a>
+      <a href={{ route('referral') }} class="nav-link block text-black">Referral Program</a>
+      <a href={{ route('contact') }} class="nav-link block text-black">Contact Us</a>
 
       
     </div>
