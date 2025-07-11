032844066e55eedd32a3e3df04ab0a1e34ebe186
parth_thaker <parththaker1991@gmail.com>
2025-07-11 21:23:21 +0530

    inner page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,12 +8,12 @@
           <div class="slide-inner slide-bg-image" data-background="images/slider1.svg">
             <div class="container">
               <div data-swiper-parallax="300" class="slide-title">
-                <h2>Lorem ipsome</h2>
+                <h2 class="text-base md:text-2xl">Lorem ipsome</h2>
               </div>
 
               <div class="clearfix"></div>
               <div data-swiper-parallax="500" class="slide-btns">
-                <button class="mt-6 bg-secondary text-white py-2 rounded-full cursor-pointer hover:bg-primary transition px-5 py-2">Register
+                <button class="mt-6 bg-secondary text-white rounded-full cursor-pointer hover:bg-primary transition px-5 py-2">Register
                   now</button>
               </div>
             </div>
@@ -31,7 +31,7 @@
 
               <div class="clearfix"></div>
               <div data-swiper-parallax="500" class="slide-btns">
-                <button class="mt-6 bg-secondary text-white py-2 rounded-full cursor-pointer hover:bg-primary transition px-5 py-2">Register
+                <button class="mt-6 bg-secondary text-white rounded-full cursor-pointer hover:bg-primary transition px-5 py-2">Register
                   now</button>
               </div>
             </div>
@@ -68,7 +68,7 @@
           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
         </p>
         <a href="#"
-          class="mt-6 bg-secondary text-white py-2 rounded-full cursor-pointer hover:bg-primary transition px-5 py-2">
+          class="mt-6 bg-secondary text-white  rounded-full cursor-pointer hover:bg-primary transition px-5 py-2">
           More Details
         </a>
       </div>
@@ -339,7 +339,7 @@
                   class="w-full border border-secondary rounded-lg px-4 py-2 focus:outline-none focus:ring-1 focus:ring-primary"></textarea>
               </div>
               <button type="submit"
-                class="mt-6 bg-secondary text-white py-2 rounded-full cursor-pointer hover:bg-primary transition px-5 py-2">Submit</button>
+                class="mt-6 bg-secondary text-white rounded-full cursor-pointer hover:bg-primary transition px-5 py-2">Submit</button>
             </form>
           </div>
         </div>
